91ab6e200eaa5995c04c3b3134d0fbdadc19ba8e
iamjaimindamor <76213871+iamjaimindamor@users.noreply.github.com>
2024-05-14 10:03:37 +0530

    Add files via upload

---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Alps Bank ");
+            Console.WriteLine("==========================");
+            
+            custInfo c1 = new custInfo();
+
+            Console.WriteLine("Enter Account Number ");
+            long acc = long.Parse(Console.ReadLine());
+            c1.findAcc(acc);
+
+
+            if (c1.findAcc(acc) == 0)
+            {
+                Console.WriteLine("Enter The Amount To Be Deposited ");
+                Console.WriteLine();
+                double amt = double.Parse(Console.ReadLine());
+                c1.deposit(amt);
+                Console.WriteLine();
+
+                Console.WriteLine("Your New Balance Is ");
+                Console.WriteLine(c1.displayBal);
+
+                Console.WriteLine();
+                Console.WriteLine("Withdrawal Amount : ");
+                double wdamt = double.Parse(Console.ReadLine());
+                c1.withdraw(wdamt);
+                Console.WriteLine();
+            }
+            else 
+            {
+                Console.WriteLine("Account Not Found");
+                Console.WriteLine("Thank You");
+            }
+
+                //Console.WriteLine("Thank You For Banking With us");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Bank/Bank/custInfo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class custInfo
+    {
+        // fields
+        private double balance;
+        private long AccountNumber = 12345678;
+        private bool IsLocked = true;
+
+        //methods
+
+        public int findAcc(long accNum)
+        {
+            if(accNum == this.AccountNumber)
+            {
+                if (this.IsLocked) { IsLocked = false; }
+                return 0; 
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public double displayBal
+        {
+            get { return balance; }
+        }
+
+        public void deposit(double amount)
+        {
+
+            if(!IsLocked) { balance = amount; }
+        }
+
+        public void withdraw(double wdrwamount)
+        {
+           
+if (!IsLocked)
+            {
+                if (wdrwamount < balance)
+                {
+                    double newBal = this.balance - wdrwamount;
+                    this.balance = newBal;
+                    Console.WriteLine($"AMOUNT {wdrwamount} WIHDRAWN");
+                    Console.WriteLine();
+                    Console.WriteLine("Want To See Updated Account Balance ? (yes/no) ");
+                    string consult = Console.ReadLine();
+                    if (String.Equals(consult, "yes", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Your Updated Balance is : {0}", displayBal);
+                        Console.WriteLine() ;
+                        Console.WriteLine("Thank You For Banking With Us ! Visit Again..");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thank You For Banking With Us ! Visit Again..");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient Balance");
+                    Console.WriteLine();
+                    Console.WriteLine("Thank You For Banking With Us ! Visit Again..");
+
+                }
+            }
+
+        } 
+    }
+}
